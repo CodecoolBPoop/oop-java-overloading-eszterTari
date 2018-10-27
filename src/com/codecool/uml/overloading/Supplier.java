9bd9b0ba7a05cd87228f7dbf889eb8b88c84d8da
eszterTari <tari.eszter.eng@gmail.com>
2018-10-27 13:20:52 +0200

    class is implemented

---
+++ src/com/codecool/uml/overloading/Supplier.java
@@ -0,0 +1,53 @@
+package com.codecool.uml.overloading;
+
+import java.util.List;
+
+public class Supplier {
+
+    private int id = 1;
+    private String name;
+    private String description;
+
+    public Supplier() {
+        id++;
+    }
+
+    public Supplier(String name, String description) {
+        id++;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Product> getProducts() {
+        return Product.getAllProductsBy(this);
+    }
+
+    public String toString() {
+        StringBuilder buildString = new StringBuilder();
+        buildString.append("id" + getId() + ",");
+        buildString.append("name" + getName() + ",");
+        buildString.append("description" + getDescription());
+
+        return buildString.toString();
+    }
+}
